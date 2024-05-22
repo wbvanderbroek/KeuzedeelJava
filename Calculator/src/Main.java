84c5f46f1d47d69141d92bc5c5a89b0bc3a08953
Walter_van_der_Broek <wbvanderbroek@st.noorderpoort.nl>
2024-05-22 15:28:52 +0200

    +

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -2,10 +2,14 @@ import java.util.Scanner;  // Import the Scanner class
 
 class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter first number");
 
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
+        String number1 = myObj.nextLine();
+        System.out.println("Enter second number");
+        String number2 = myObj.nextLine();
+
+        System.out.println("Your numbers are " + number1 + " and " + number2);
+
     }
 }
